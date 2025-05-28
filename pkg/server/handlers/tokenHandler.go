c08092590977fea0cee1cae2f0082037c53112a1
SergeyAnosov <saanosov@istokmw.ru>
2025-05-28 16:46:07 +0300

    feat: token 1.1

--- pkg/server/handlers/tokenHandler.go
+++ pkg/server/handlers/tokenHandler.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/golang-jwt/jwt/v5"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 type PasswordRequest struct {
@@ -32,9 +34,62 @@ func SigninHandler(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	//todo логику создания токена
-
-	WriteJson(writer, map[string]string{
-		"token": "new token",
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"password_hash": password.Password, // можно хранить хэш, но в учебном примере — сам пароль
+		"exp":           time.Now().Add(8 * time.Hour).Unix(),
 	})
+
+	signedToken, err := token.SignedString(([]byte(pwd)))
+	if err != nil {
+		SendError(writer, "не удалось получить jwt token", http.StatusInternalServerError)
+	}
+
+	WriteJson(writer, map[string]interface{}{
+		"token": signedToken,
+	})
+}
+
+func Auth(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		expectedPassword := os.Getenv("PASSWORD")
+		if expectedPassword == "" {
+			next(writer, request)
+			return
+		}
+
+		cookie, err := request.Cookie("token")
+		if err != nil {
+			SendError(writer, "требуется авторизация", http.StatusUnauthorized)
+			return
+		}
+		tokenString := cookie.Value
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return ([]byte)(expectedPassword), nil
+		})
+
+		if err != nil || !token.Valid {
+			SendError(writer, "invalid or missing token", http.StatusUnauthorized)
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			SendError(writer, "invalid token claims", http.StatusUnauthorized)
+			return
+		}
+
+		passwordHash, ok := claims["password_hash"].(string)
+		if !ok || passwordHash != expectedPassword {
+			SendError(writer, "authentification required", http.StatusUnauthorized)
+			return
+		}
+		next(writer, request)
+	}
+}
+
+func AsChiMiddleware(fn func(http.HandlerFunc) http.HandlerFunc) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(fn(next.ServeHTTP))
+	}
 }
