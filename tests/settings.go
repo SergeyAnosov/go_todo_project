7468392f3833dae927b3eb7db23a735217052ea7
SergeyAnosov <saanosov@istokmw.ru>
2025-06-02 12:32:28 +0300

    to review

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = false
 var Search = false
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDg1NDE0NTEsInBhc3N3b3JkX2hhc2giOiJzZWNyZXQifQ.d1euKpqf7LgL6SrDzqv2LIwHryQn3lXwNaUQiGbw33Q`
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDg4ODQ0MTQsInBhc3N3b3JkX2hhc2giOiJzZWNyZXQifQ.Iuv1_BI7Zs11TLu96O6xxWxf1RyBA5dgdWRo2Ph5Fjc`
